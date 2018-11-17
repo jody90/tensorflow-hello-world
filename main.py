db2bd9381d2573cbe736e1f1560daaca2d3090a1
Jody <jody.lerch@web.de>
2018-11-17 16:20:17 +0100

    Predict and Predict single added

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ import matplotlib.pyplot as plt
 
 import sys, getopt
 
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
 def main(argv) :
     inputfile = ''
     outputfile = ''
@@ -33,16 +43,6 @@ def main(argv) :
     print('Output file is: ', outputfile)
     print('Action is: ', action)
 
-    fashion_mnist = keras.datasets.fashion_mnist
-
-    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-    train_images = train_images / 255.0
-
-    test_images = test_images / 255.0
-
     if action == "new" :
         model = getModel()
         model.fit(train_images, train_labels, epochs=5)
@@ -52,13 +52,41 @@ def main(argv) :
         model = loadModel(inputfile)
         model.fit(train_images, train_labels, epochs=5)
         model.save(outputfile)
+        test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+        print('Test accuracy:', test_acc)
 
     elif action == "use" :
         model = loadModel(inputfile)
 
-    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    elif action == "predict" :
+        model = loadModel(inputfile)
 
-    print('Test accuracy:', test_acc)
+        predictions = model.predict(test_images)
+
+        # Plot the first X test images, their predicted label, and the true label
+        # Color correct predictions in blue, incorrect predictions in red
+        num_rows = 5
+        num_cols = 3
+        num_images = num_rows*num_cols
+        plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+        for i in range(num_images):
+            plt.subplot(num_rows, 2*num_cols, 2*i+1)
+            plot_image(i, predictions, test_labels, test_images)
+            plt.subplot(num_rows, 2*num_cols, 2*i+2)
+            plot_value_array(i, predictions, test_labels)
+        
+        plt.show()
+    
+    elif action == "predict-single" :
+        model = loadModel(inputfile)
+
+        img = test_images[4]
+        img = (np.expand_dims(img,0))
+        predictions_single = model.predict(img)
+        plot_value_array(0, predictions_single, test_labels)
+        _ = plt.xticks(range(10), class_names, rotation=45)
+        plt.show()
 
 def getModel() :
 
@@ -76,6 +104,37 @@ def getModel() :
 
 def loadModel(filename) :
     return keras.models.load_model(filename)
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+  
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1]) 
+  predicted_label = np.argmax(predictions_array)
+ 
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
 
 # train_images.shape
 
